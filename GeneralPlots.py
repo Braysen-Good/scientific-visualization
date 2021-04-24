e9e972c6372da20bd70ecd05e3f4c829bb00b8cb
Braysen Goodwin <braysen@infinidlearning.com>
2021-04-23 18:00:15 -0600

    added function documentations

--- GeneralPlots.py
+++ GeneralPlots.py
@@ -2,6 +2,7 @@ from Parser import Parser
 
 import numpy as np
 import matplotlib.pyplot as plt
+import constants
 class GeneralPlot:
 
     def __init__(self, fm: Parser):
@@ -11,21 +12,25 @@ class GeneralPlot:
         self.healthyCount = np.zeros((self.framecount))
         self.starvedCount = np.zeros((self.framecount))
         self.deadCount = np.zeros((self.framecount))
+        self.apoptoticDeathCount = np.zeros((self.framecount))
         
         for i in range(self.framecount):
             cellCollection = fm.getFrame(i).cells
             variables = cellCollection.variables
             cells = cellCollection.data
-            self.totalPop[i]=cells[0].size
-            self.healthyCount[i]=np.count_nonzero(cells[variables['cycle_model']] == 5)
-            self.starvedCount[i]=np.count_nonzero(cells[variables['cycle_model']] == 101)
-            self.deadCount[i]=np.count_nonzero(cells[variables['cycle_model']] == 100)
+            self.totalPop[i] = cells[0].size
+            cellModel = cells[variables['cycle_model']]
+            self.healthyCount[i] = np.count_nonzero(cellModel < constants.apoptosis_death_model)
+            self.starvedCount[i] = np.count_nonzero(cellModel > constants.apoptosis_death_model)
+            self.apoptoticDeathCount[i] = np.count_nonzero(cellModel == constants.apoptosis_death_model)
+            self.deadCount[i] = np.count_nonzero(cellModel >= constants.apoptosis_death_model)
             
     def plotPop(self):
-        plt.plot(self.totalPop, color = "c", label='total population')
-        plt.plot(self.healthyCount,color = "b", label='healthy cells')
-        plt.plot(self.starvedCount,color = "r",  label='starved cells')
-        plt.plot(self.deadCount,color = "k", label='dead cells')
+        plt.plot(self.totalPop, color = "c", label = 'total population')
+        plt.plot(self.healthyCount, color = "g", label = 'healthy cells')
+        plt.plot(self.starvedCount, color = "r",  label = 'starved cells (dead)')
+        plt.plot(self.apoptoticDeathCount, color = "tab:orange", label='apoptotic cells (dead)')
+        plt.plot(self.deadCount, color = "k", label = 'dead cells (total)')
         plt.ylabel('# of cells')
         plt.xlabel('frame number')
         plt.legend()
---
+++ constants.py
@@ -0,0 +1,51 @@
+# constants from the contant header file from Physicell `PhysiCell/core/PhysiCell_constants.h`
+
+# currently recognized cell cycle models 
+advanced_Ki67_cycle_model = 0
+basic_Ki67_cycle_model = 1
+flow_cytometry_cycle_model = 2
+live_apoptotic_cycle_model = 3
+total_cells_cycle_model = 4
+live_cells_cycle_model = 5 
+flow_cytometry_separated_cycle_model = 6 
+cycling_quiescent_model = 7 
+
+# currently recognized death models 
+apoptosis_death_model = 100 
+necrosis_death_model = 101 
+autophagy_death_model = 102 
+
+custom_cycle_model = 9999
+
+# currently recognized cell cycle and death phases and cycle phases
+Ki67_positive_premitotic = 0
+Ki67_positive_postmitotic = 1
+Ki67_positive = 2
+Ki67_negative = 3
+G0G1_phase = 4
+G0_phase = 5
+G1_phase = 6 
+G1a_phase = 7 
+G1b_phase = 8
+G1c_phase = 9
+S_phase = 10
+G2M_phase = 11
+G2_phase = 12
+M_phase = 13
+live = 14
+	
+G1pm_phase = 15
+G1ps_phase = 16
+	
+cycling = 17
+quiescent = 18
+	
+	
+custom_phase = 9999
+
+# death phases
+apoptotic = 100
+necrotic_swelling = 101
+necrotic_lysed = 102
+necrotic = 103
+debris = 104
